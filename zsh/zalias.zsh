84d626a907e0d91ee05e660305942f3335f05539
R C <xrobinc@gmail.com>
2012-08-11 15:27:11 -0700

    modified:   beetsconfig 	modified:   i3/config 	new file:   vim/colors/hybrid.vim 	modified:   weechat/plugins.conf 	modified:   weechat/weeget/plugins.xml.gz 	modified:   zsh/zalias.zsh

--- zsh/zalias.zsh
+++ zsh/zalias.zsh
@@ -18,6 +18,7 @@ alias sprunge=" curl -F 'sprunge=<-' http://sprunge.us"
 alias starcraft="wine /home/robin/.wine/drive_c/Program\ Files/StarCraft\ II/StarCraft\ II.exe>/dev/null 2>&1 &"
 alias steam="wine /home/robin/.wine-steam/drive_c/Program\ Files/Steam/Steam.exe>/dev/null 2>&1 &"
 
+alias webcam="mplayer tv:// -tv driver=v4l2:width=640:height=480:device=/dev/video0 -fps 30 -vf screenshot"
 alias omup="omup -s"
 
 alias moonlightsucks="sudo qemu-kvm -hda ~/kvm/windows -m 6144 -smp 2 -vga std"
@@ -25,3 +26,4 @@ alias moonlightsucks="sudo qemu-kvm -hda ~/kvm/windows -m 6144 -smp 2 -vga std"
 #~/github/bin/
 alias screenshot="~/github/scripts/screenshot"
 alias imgur="~/github/scripts/imgur"
+
