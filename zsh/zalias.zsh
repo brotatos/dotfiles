b42d2339da14a38fc7cd11ea167df211432825fe
r c <9tumor@gmail.com>
2012-08-13 19:57:55 -0700

    deleted:    zlogin 	renamed:    zalias -> zsh/zalias.zsh 	new file:   zsh/zstyles.zsh

--- zsh/zalias.zsh
+++ zsh/zalias.zsh
@@ -8,18 +8,18 @@ alias reconnect="sudo netcfg -r home"
 alias shutdown="sudo systemctl poweroff"
 alias reboot="sudo systemctl reboot"
 
-alias pinstall="sudo pacman -S"
-alias update="sudo pacman -Syu"
-alias updatef="sudo pacman -Syyuu"
-alias updatee="sudo pacman -Syu; cower -uv"
+alias update="sudo pacman -Syu; meat -u"
 
-alias sprunge="curl -F 'sprunge=<-' http://sprunge.us"
+alias sprunge=" curl -F 'sprunge=<-' http://sprunge.us"
 
 alias starcraft="wine /home/robin/.wine/drive_c/Program\ Files/StarCraft\ II/StarCraft\ II.exe>/dev/null 2>&1 &"
-alias steam="wine /home/robin/.wine-steam/drive_c/Program\ Files/Steam/Steam.exe>/dev/null 2>&1 &"
 
+alias webcam="mplayer tv:// -tv driver=v4l2:width=640:height=480:device=/dev/video0 -fps 30 -vf screenshot"
 alias omup="omup -s"
+
+alias moonlightsucks="sudo qemu-kvm -hda ~/kvm/windows -m 6144 -smp 2 -vga std"
 
 #~/github/bin/
 alias screenshot="~/github/scripts/screenshot"
 alias imgur="~/github/scripts/imgur"
+alias garbage="~/github/scripts/garbage"
