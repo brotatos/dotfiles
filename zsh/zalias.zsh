554340bfe6e82011e9ac4a896219f51e3a85402f
r c <9tumor@gmail.com>
2012-08-21 21:26:46 -0700

    latex and alias junk

--- zsh/zalias.zsh
+++ zsh/zalias.zsh
@@ -1,27 +1,31 @@
 #!/bin/zsh
+
+# general
 alias ls='ls --color=auto'
 alias grep='grep --colour=auto'
 
-alias gc="ping -c 3 google.com"
-
-alias reconnect="sudo netcfg -r home" 
 alias shutdown="sudo systemctl poweroff"
 alias reboot="sudo systemctl reboot"
-
-alias update="sudo pacman -Syu; meat -u"
-
-alias sprunge=" curl -F 'sprunge=<-' http://sprunge.us"
-
-alias starcraft="wine /home/robin/.wine/drive_c/Program\ Files/StarCraft\ II/StarCraft\ II.exe>/dev/null 2>&1 &"
-#alias steam="wine /home/robin/.wine-steam/drive_c/Program\ Files/Steam/Steam.exe >/dev/null 2>&1 &"
-alias steam="wine /home/robin/.wine-steam/drive_c/Program\ Files/Steam/Steam.exe -no-dwrite>/dev/null 2>&1 &"
 
 alias webcam="mplayer tv:// -tv driver=v4l2:width=640:height=480:device=/dev/video0 -fps 30 -vf screenshot"
 alias omup="omup -s"
 
-alias moonlightsucks="sudo qemu-kvm -hda ~/kvm/windows -m 6144 -smp 2 -vga std"
+# arch
+alias update="sudo pacman -Syu; meat -u"
+alias sprunge=" curl -F 'sprunge=<-' http://sprunge.us"
 
-#~/github/bin/
+# ~/github/bin/
 alias screenshot="~/github/scripts/screenshot"
 alias imgur="~/github/scripts/imgur"
 alias garbage="~/github/scripts/garbage"
+
+# latex
+alias ct="rm *.{pdf,aux,dvi}"
+
+# steam
+alias starcraft="wine /home/robin/.wine/drive_c/Program\ Files/StarCraft\ II/StarCraft\ II.exe>/dev/null 2>&1 &"
+#alias steam="wine /home/robin/.wine-steam/drive_c/Program\ Files/Steam/Steam.exe >/dev/null 2>&1 &"
+alias steam="wine /home/robin/.wine-steam/drive_c/Program\ Files/Steam/Steam.exe -no-dwrite>/dev/null 2>&1 &"
+
+# kvm
+alias moonlightsucks="sudo qemu-kvm -hda ~/kvm/windows -m 6144 -smp 2 -vga std"
