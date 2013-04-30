1455483216c43efe137067c8e731638bc36d298c
Robin Choudhury <xrobinc@gmail.com>
2013-04-30 10:28:38 -0700

    modified:   Xresources 	modified:   i3/config 	modified:   tmux.conf 	modified:   xinitrc 	modified:   zsh/zalias.zsh

--- zsh/zalias.zsh
+++ zsh/zalias.zsh
@@ -9,7 +9,7 @@ alias ls='ls --group-directories-first --color=auto'
 alias more='/usr/bin/less'
 alias grep='grep --colour=auto'
 alias ...='cd ..'
-alias fixr='xrandr --output LVDS --mode 1600x900'
+alias fixr='xrandr --output LVDS1 --mode 1600x900'
 
 alias shutdown="systemctl poweroff"
 alias reboot="systemctl reboot"
@@ -18,7 +18,7 @@ alias suspend="systemctl suspend"
 alias webcam="mplayer tv:// -tv driver=v4l2:width=640:height=480:device=/dev/video0 -fps 30 -vf screenshot"
 alias omup="omup -s"
 
-alias reconnect="sudo netcfg -R wlan0"
+alias reconnect="sudo netctl restore"
 alias gc="ping -c 3 google.com"
 
 # arch
