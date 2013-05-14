3d49d7a72367ae23b0c7a31d19cdea5f0e17cee7
Robin Choudhury <xrobinc@gmail.com>
2013-05-14 08:27:09 -0700

    Remove comments in setup script.
    modified:   setup.sh

--- setup.sh
+++ setup.sh
@@ -3,25 +3,8 @@
 #ln -s source_file link_name
 
 files=('bash_profile' 'bashrc' 'rtorrent.rc' 'vim' 'xinitrc' 'Xresources' 'mpd' 'mplayer' 'ncmpcpp' 'beetsconfig' 'mpdscribble' 'i3' 'livestreamerrc' 'mutt' 'weechat' 'vimrc' 'tmux.conf' 'bash_alias' 'i3status.conf' 'zshrc' 'vimperatorrc' 'zprofile')
-forced=true
 
-# itarate over all arguments
-#for i in "$@"
-#do
-# if [ $i = "f" ]; then
-# forced=true
-# fi
-#done
-#
-#for i in "${files[@]}"
-#do
-# #if $forced ; then
-# # rm ~/.$i
-# #fi
-# cp -r ~/.$i ~/.dotfiles/$i
-#done
-for i in "${files[@]}"
-do
+for i in "${files[@]}"; do
     rm -r ~/.$i
     ln -s ~/github/dotfiles/$i ~/.$i
 done
