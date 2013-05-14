993f5d090e0e961edbf126b458c571e81db6754d
Robin Choudhury <xrobinc@gmail.com>
2013-05-14 09:03:24 -0700

    Switched to pentadactyl.
    new file:   pentadactylrc
    modified:   setup.sh
    modified:   vimperatorrc

--- setup.sh
+++ setup.sh
@@ -2,7 +2,10 @@
 # Sets up the appropriate symlinks for the config files located here
 #ln -s source_file link_name
 
-files=('bash_profile' 'bashrc' 'rtorrent.rc' 'vim' 'xinitrc' 'Xresources' 'mpd' 'mplayer' 'ncmpcpp' 'beetsconfig' 'mpdscribble' 'i3' 'livestreamerrc' 'mutt' 'weechat' 'vimrc' 'tmux.conf' 'bash_alias' 'i3status.conf' 'zshrc' 'vimperatorrc' 'zprofile')
+files=('bash_profile' 'bashrc' 'rtorrent.rc' 'vim' 'xinitrc'
+'Xresources' 'mpd' 'mplayer' 'ncmpcpp' 'beetsconfig' 'mpdscribble' 'i3'
+'livestreamerrc' 'mutt' 'weechat' 'vimrc' 'tmux.conf' 'bash_alias'
+'i3status.conf' 'zshrc' 'vimperatorrc' 'zprofile' 'pentadactylrc')
 
 for i in "${files[@]}"; do
     rm -r ~/.$i
