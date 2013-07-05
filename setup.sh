5a04fdf490b47ef55036af40ffb0c604667d188a
Robin Choudhury <xrobinc@gmail.com>
2013-07-04 18:42:28 -0700

    Switch to termite.

--- setup.sh
+++ setup.sh
@@ -2,15 +2,18 @@
 # Sets up the appropriate symlinks for the config files located here
 #ln -s source_file link_name
 
-files=('bash_profile' 'bashrc' 'rtorrent.rc' 'vim' 'xinitrc'
-'Xresources' 'mpd' 'mplayer' 'ncmpcpp' 'beetsconfig' 'mpdscribble' 'i3'
-'livestreamerrc' 'mutt' 'weechat' 'vimrc' 'tmux.conf' 'vimperatorrc'
-'bash_alias' 'i3status.conf' 'zshrc' 'zsh' 'zprofile' 'libao'
-'pentadactylrc')
-forced=true
+files=('bash_profile' 'bashrc' 'rtorrent.rc' 'vim' 'xinitrc' 'Xresources' 'mpd'
+'mplayer' 'ncmpcpp' 'beetsconfig' 'mpdscribble' 'i3' 'livestreamerrc' 'mutt'
+'weechat' 'vimrc' 'tmux.conf' 'bash_alias' 'i3status.conf' 'zshrc'
+'vimperatorrc' 'zprofile' 'pentadactylrc')
 
-for i in "${files[@]}"
-do
+for i in "${files[@]}"; do
     rm -r ~/.$i
     ln -s ~/github/dotfiles/$i ~/.$i
 done
+
+if [ ! -d ~/.config/termite ]; then
+   mkdir -p ~/.config/termite
+fi
+
+ln -s ~/github/dotfiles/termite.cfg ~/.config/termite/config
