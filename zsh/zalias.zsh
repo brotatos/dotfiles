ff8c03d444627423a29b7f7eabbdfbbf14289679
Robin Choudhury <xrobinc@gmail.com>
2013-07-05 22:46:07 -0700

    Remove laptop specific aliases.

--- zsh/zalias.zsh
+++ zsh/zalias.zsh
@@ -1,16 +1,11 @@
-#!/bin/zsh
-
-#thinkpad
-alias tg="/usr/bin/touchpadtoggle"
-
 # general
 alias g="git"
 alias l='ls --group-directories-first --color=auto'
+alias l='ls --group-directories-first --color=auto -lash'
 alias ls='ls --group-directories-first --color=auto'
 alias more='/usr/bin/less'
 alias grep='grep --colour=auto'
 alias ...='cd ..'
-alias fixr='xrandr --output LVDS1 --mode 1600x900'
 
 alias shutdown="systemctl poweroff"
 alias reboot="systemctl reboot"
@@ -28,8 +23,3 @@ alias update="sudo pacman --color=always -Syu; meat -u"
 alias sprunge="curl -F 'sprunge=<-' http://sprunge.us"
 alias sickbeard="sudo systemctl start sickbeard.service"
 alias sickbeards="sudo systemctl stop sickbeard.service"
-
-# school
-alias home="sudo netctl start home"
-alias smw="sudo netctl start smw"
-alias cplan="sudo netctl start cplan"
