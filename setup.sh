9edaea00f6b93a8e6bdf946f7a72459ff1e6453d
Robin Choudhury <xrobinc@gmail.com>
2013-08-12 09:39:16 -0700

    Add mpv.

--- setup.sh
+++ setup.sh
@@ -2,11 +2,10 @@
 # Sets up the appropriate symlinks for the config files located here
 #ln -s source_file link_name
 
-files=('bash_profile' 'bashrc' 'rtorrent.rc' 'vim' 'xinitrc'
-'Xresources' 'mpd' 'mplayer' 'ncmpcpp' 'beetsconfig' 'mpdscribble' 'i3'
-'livestreamerrc' 'mutt' 'weechat' 'vimrc' 'tmux.conf' 'bash_alias'
-'i3status.conf' 'zsh' 'zshrc' 'vimperatorrc' 'zprofile' 'pentadactylrc'
-'xbindkeysrc')
+files=('bash_profile' 'bashrc' 'rtorrent.rc' 'vim' 'xinitrc' 'Xresources' 'mpd'
+'mplayer' 'ncmpcpp' 'beetsconfig' 'mpdscribble' 'i3' 'livestreamerrc' 'mutt'
+'mpv' 'weechat' 'vimrc' 'tmux.conf' 'bash_alias' 'i3status.conf' 'zsh' 'zshrc'
+'vimperatorrc' 'zprofile' 'pentadactylrc' 'xbindkeysrc')
 
 for i in "${files[@]}"; do
    rm -r "~/.$i"
