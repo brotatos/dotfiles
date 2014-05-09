840b1438c29723ba53b4de7daaa7001459fad2f8
Robin Choudhury <xrobinc@gmail.com>
2014-05-09 08:11:02 -0700

    Fish: Add default colors.

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,3 +1,35 @@
+# Regular syntax highlighting colors
+set fish_color_normal normal
+set fish_color_param 00afff cyan
+set fish_color_redirection normal
+set fish_color_comment red
+set fish_color_error red --bold
+set fish_color_escape cyan
+set fish_color_operator cyan
+set fish_color_quote brown
+set fish_color_autosuggestion 555 yellow
+set fish_color_valid_path --underline
+
+set fish_color_cwd green
+set fish_color_cwd_root red
+
+# Background color for matching quotes and parenthesis
+set fish_color_match cyan
+
+# Background color for search matches
+set fish_color_search_match --background=purple
+
+# Pager colors
+set fish_pager_color_prefix cyan
+set fish_pager_color_completion normal
+set fish_pager_color_description 555 yellow
+set fish_pager_color_progress cyan
+
+#
+# Directory history colors
+#
+set fish_color_history_current cyan
+
 # Set the color command to pink.
 set fish_color_command d787ff
 set -u fish_user_paths $fish_user_paths ~/github/scripts ~/.gem/ruby/2.0.0/bin
