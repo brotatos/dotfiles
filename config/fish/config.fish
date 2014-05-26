982972651077706e511d29dd3192ae6361eccf59
Robin Choudhury <xrobinc@gmail.com>
2014-05-26 16:26:46 -0700

    Termite: Increase font.

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -16,6 +16,37 @@ end
 if test -d /usr/bin/core_perl
    set -x PATH $PATH /usr/bin/core_perl
 end
+# Regular syntax highlighting colors
+set fish_color_normal normal
+set fish_color_param 00afff cyan
+set fish_color_redirection normal
+set fish_color_comment red
+set fish_color_error red --bold
+set fish_color_escape cyan
+set fish_color_operator cyan
+set fish_color_quote brown
+set fish_color_autosuggestion 555 yellow
+set fish_color_valid_path --underline
+
+set fish_color_cwd green
+set fish_color_cwd_root red
+
+# Background color for matching quotes and parenthesis
+set fish_color_match cyan
+
+# Background color for search matches
+set fish_color_search_match --background=purple
+
+# Pager colors
+set fish_pager_color_prefix cyan
+set fish_pager_color_completion normal
+set fish_pager_color_description 555 yellow
+set fish_pager_color_progress cyan
+
+#
+# Directory history colors
+#
+set fish_color_history_current cyan
 
 set fish_color_command d787ff
 set -u fish_user_paths $fish_user_paths ~/github/scripts ~/.gem/ruby/2.0.0/bin
@@ -213,7 +244,21 @@ function compareOut
    colordiff mine.Z his.Z
 end function
 
-function comp
+function compareT
+   ./Compress -rsct $argv > mine
+   ./StaleyLZWCmp -rsct $argv > his
+   colordiff mine his
+end function
+
+function compareOut
+   ./Compress $argv
+   mv $argv.Z $argv.out
+   ./StaleyLZWCmp $argv
+   mv $argv.Z $argv.exp
+   colordiff $argv.out $argv.exp
+end function
+
+function compareLZW
    make
    ./Compress -s $argv > mine
    ./StaleyCompress -s $argv > his
@@ -230,4 +275,12 @@ end function
 function timeit
    make
    bash -c "(time ./StaleyStress) > curTime 2>&1"
+end function
+
+function comp
+   make
+   echo -e "\nmine:"
+   ./Compress -s $argv
+   echo -e "\nStaley:"
+   ./StaleyCompress -s $argv
 end function
