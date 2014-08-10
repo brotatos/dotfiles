b1661bc4712e1609fb165b5a7c59ce9114eccd62
Robin Choudhury <xrobinc@gmail.com>
2014-08-09 21:54:08 -0700

    Add git alias to remove locally merged branches.

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -323,3 +323,8 @@ end function
 function guest
    sudo netctl start wlan0-Guest
 end function
+
+# git
+function remove-branches
+   git branch --merged master | grep -v "\* master" | xargs -n 1 git branch -d
+end function
