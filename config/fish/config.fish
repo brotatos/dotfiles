a8936d829c0fbfbfd4aceabac9cd09c73288b5c0
Robin Choudhury <xrobinc@gmail.com>
2014-08-12 17:15:45 -0700

    Remove ansi dircolors and add incognito mode to i3.

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -336,12 +336,6 @@ function _dircolors_set_256_color
     end
 end
 
-function _dircolors_set_ansi_color
-    if type -f dircolors > /dev/null and type -f ~/.dircolors.ansi > /dev/null
-        eval (dircolors -c ~/.dircolors.ansi | sed 's/>&\/dev\/null$//')
-    end
-end
-
 switch (echo $TERM)
     case linux
         _dircolors_set_ansi_color
