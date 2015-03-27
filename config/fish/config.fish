bdcae0fc0de6d27526fed85c0a88c5ceaf5a8d08
Robin Choudhury <xrobinc@gmail.com>
2015-03-26 21:18:30 -0700

    Remove unneeded path.

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -51,7 +51,6 @@ set fish_pager_color_progress cyan
 set fish_color_history_current cyan
 
 set fish_color_command d787ff
-set -u fish_user_paths $fish_user_paths ~/github/scripts ~/.gem/ruby/2.0.0/bin
 set -x EDITOR vim
 set -x VISUAL vim
 set -x BROWSER chromium
