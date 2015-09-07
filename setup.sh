96ae7cb63d3ad20f4ab15e2f93f98136777027b6
Robin Choudhury <xrobinc@gmail.com>
2015-09-06 21:54:12 -0700

    Add bash prompt to dotfiles repo.

--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@
 # Sets up the appropriate symlinks for the config files located here
 #ln -s source_file link_name
 
-files=('bash_profile' 'bashrc' 'rtorrent.rc' 'xinitrc' 'Xresources' 'mpd'
+files=('bash_profile' 'bash_prompt' 'bashrc' 'rtorrent.rc' 'xinitrc' 'Xresources' 'mpd'
 'mplayer' 'ncmpcpp'  'mpdscribble' 'i3' 'livestreamerrc' 'mutt' 'weechat'
 'tmux.conf' 'bash_alias' 'i3status.conf' 'zshrc' 'vimperatorrc' 'zprofile'
 'pentadactylrc' 'mpv')
